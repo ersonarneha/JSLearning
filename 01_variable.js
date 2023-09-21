1e25dce6922bfc1a6514e96d066797cee09309fe
ersonarneha <ersonarneha93@gmail.com>
2023-09-21 22:22:55 +0530

    variable assignment
    variable assignment

---
+++ 01_variable.js
@@ -0,0 +1,20 @@
+
+var firstName;  //variable declaration
+firstName = "Neha";  //variable initialization
+console.log(firstName);
+firstName="Mrs.Neha";  //variable update
+console.log(firstName);
+
+var lastName = "Sonar";  //variable declaration and initialization
+console.log(lastName);
+
+var height; //variable declaration
+console.log(height);
+height="6 ft";  //initialization
+console.log(height);
+
+height="6.2 ft";  //update
+console.log(height);
+
+var marks = "78%";
+console.log('10th marks:',marks,"Grade A");
---
+++ 01_variableAssign.js
@@ -0,0 +1,35 @@
+console.log("------------step 1-----------");
+var myFullName;
+console.log(myFullName);
+
+myFullName="Neha Sonar";
+console.log(myFullName);
+
+myFullName="Neha Suresh Sonar";
+console.log(myFullName);
+
+console.log("-----------step 2-----------");
+var myLove="I love only JS";
+console.log(myLove);
+
+console.log("----------step 3-------");
+var firstName="Neha";
+console.log("First name =",firstName);
+var lastName="Sonar";
+console.log("Last name =",lastName);
+var age="30";
+console.log("Age=",age);
+var address="Moshi,Pune";
+console.log("Address=",address);
+var pinCode="412105";
+console.log("pincode=",pinCode);
+
+var gender="Female";
+console.log("Gender=",gender);
+var isMarried="Yes";
+console.log("Is married?",isMarried);
+
+
+
+
+
